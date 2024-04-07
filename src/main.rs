58e26674adeaaefbe8c723d539893ec2d5920048
Lewis <lsm10100@gmail.com>
2024-04-07 22:10:27 +0100

    handle left / right input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::time::Duration;
+use blue_engine::KeyCode;
 
 use crate::game_engine_setup::{FramerateLimiter, GameProgressLimiter, setup_engine};
 use crate::render_block::render_block;
@@ -18,12 +19,21 @@ fn main() {
     let mut frame_rate_limiter = FramerateLimiter::new(Duration::from_secs(1/60));
     let mut game_progress_limiter = GameProgressLimiter::new(Duration::from_secs(1));
     rendering_engine
-        .update_loop(move |renderer, _window, objects, _input, _camera, _plugins| {
+        .update_loop(move |renderer, _window, objects, input, _camera, _plugins| {
+            if input.key_pressed(KeyCode::ArrowLeft) {
+                tetris_engine.move_left();
+            }
+
+            if input.key_pressed(KeyCode::ArrowRight) {
+                tetris_engine.move_right();
+            }
+
+            tetris_engine.generate_blocks().iter().for_each(|block|{
+                render_block(block, objects, renderer);
+            });
+
             frame_rate_limiter.tick(||{
                 game_progress_limiter.tick(||{
-                    tetris_engine.generate_blocks().iter().for_each(|block|{
-                        render_block(block, objects, renderer);
-                    });
                     tetris_engine.tick();
                 })
             })
