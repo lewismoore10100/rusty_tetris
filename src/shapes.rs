49751cf09f0cb8ab86f7a864666ec2c28aeb1710
Lewis <lsm10100@gmail.com>
2024-04-22 21:19:34 +0100

    refactor

--- src/shapes.rs
+++ src/shapes.rs
@@ -42,31 +42,34 @@ impl Square {
 }
 
 pub trait PlayableShape {
-    fn blocks(&self) -> &[TetrisBlock];
-
-    fn drain_to(&mut self, merge_to: &mut Vec<TetrisBlock>);
-
-    fn move_down(&mut self);
-
-    fn move_left(&mut self);
-    fn move_right(&mut self);
-}
-impl PlayableShape for Square {
     fn blocks(&self) -> &[TetrisBlock] {
-        &self.block_group.blocks[..]
+        &self.block_group().blocks[..]
     }
     fn drain_to(&mut self, merge_to: &mut Vec<TetrisBlock>){
-        self.block_group.drain_to(merge_to)
+        self.block_group_mut().drain_to(merge_to)
     }
 
     fn move_down(&mut self) {
-        self.block_group.move_down()
+        self.block_group_mut().move_down()
     }
 
     fn move_left(&mut self) {
-        self.block_group.move_left()
+        self.block_group_mut().move_left()
     }
     fn move_right(&mut self) {
-        self.block_group.move_right()
+        self.block_group_mut().move_right()
+    }
+
+    fn block_group_mut(&mut self) -> &mut BlockGroup;
+
+    fn block_group(&self) -> &BlockGroup;
+}
+impl PlayableShape for Square {
+    fn block_group_mut(&mut self) -> &mut BlockGroup {
+        &mut self.block_group
+    }
+
+    fn block_group(&self) -> &BlockGroup {
+        &self.block_group
     }
 }
