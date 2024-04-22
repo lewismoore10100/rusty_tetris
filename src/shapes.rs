00ea826bd84d48614ebf51f1b2ed77073e0786cc
Lewis <lsm10100@gmail.com>
2024-04-22 21:10:59 +0100

    use composition instead of inheritance

--- src/shapes.rs
+++ src/shapes.rs
@@ -1,48 +1,72 @@
 use crate::tetris_block::TetrisBlock;
 
-pub trait PlayableShape {
-    fn blocks(&self) -> &[TetrisBlock];
-    fn blocks_mut(&mut self) -> &mut Vec<TetrisBlock>;
-    fn rotate(&mut self);
+pub struct BlockGroup {
+    pub blocks: Vec<TetrisBlock>,
+}
+
+impl BlockGroup {
     fn drain_to(&mut self, merge_to: &mut Vec<TetrisBlock>) {
-        merge_to.append(&mut self.blocks_mut());
+        merge_to.append(&mut self.blocks);
     }
 
     fn move_down(&mut self) {
-        self.blocks_mut().iter_mut().for_each(|b| b.y -= 1)
+        self.blocks.iter_mut().for_each(|b| b.y -= 1)
     }
 
     fn move_left(&mut self) {
-        self.blocks_mut().iter_mut().for_each(|b| b.x -= 1)
+        self.blocks.iter_mut().for_each(|b| b.x -= 1)
     }
-
     fn move_right(&mut self) {
-        self.blocks_mut().iter_mut().for_each(|b| b.x += 1)
+        self.blocks.iter_mut().for_each(|b| b.x += 1)
     }
 }
 
+
 pub struct Square {
-    pub blocks: Vec<TetrisBlock>,
+    pub blocks: BlockGroup
 }
 
 impl Square {
     pub(crate) fn new() -> Square {
         Square {
-            blocks: vec![
-                TetrisBlock::new(4, 19),
-                TetrisBlock::new(5, 19),
-                TetrisBlock::new(4, 18),
-                TetrisBlock::new(5, 18),
-            ]
+            blocks: BlockGroup {
+                blocks: vec![
+                    TetrisBlock::new(4, 19),
+                    TetrisBlock::new(5, 19),
+                    TetrisBlock::new(4, 18),
+                    TetrisBlock::new(5, 18),
+                ]
+            }
         }
     }
 }
+
+pub trait PlayableShape {
+    fn blocks(&self) -> &[TetrisBlock];
+
+    fn drain_to(&mut self, merge_to: &mut Vec<TetrisBlock>);
+
+    fn move_down(&mut self);
+
+    fn move_left(&mut self);
+    fn move_right(&mut self);
+}
 impl PlayableShape for Square {
     fn blocks(&self) -> &[TetrisBlock] {
-        &self.blocks[..]
+        &self.blocks.blocks[..]
     }
-    fn blocks_mut(&mut self) -> &mut Vec<TetrisBlock> {
-        &mut self.blocks
+    fn drain_to(&mut self, merge_to: &mut Vec<TetrisBlock>){
+        self.blocks.drain_to(merge_to)
     }
-    fn rotate(&mut self) {}
+
+    fn move_down(&mut self) {
+        self.blocks.move_down()
+    }
+
+    fn move_left(&mut self) {
+        self.blocks.move_left()
+    }
+    fn move_right(&mut self) {
+        self.blocks.move_right()
+    }
 }
