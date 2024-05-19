6620b4ae954dcf76bc429a48272f237cca3ee975
Lewis <lsm10100@gmail.com>
2024-05-19 23:00:22 +0100

    lower cpu usage

--- src/game_engine_setup.rs
+++ src/game_engine_setup.rs
@@ -1,3 +1,4 @@
+use std::thread::{sleep, Thread};
 use std::time::{Duration, Instant};
 
 use blue_engine::{Engine, WindowDescriptor};
@@ -5,6 +6,7 @@ use blue_engine::{Engine, WindowDescriptor};
 pub struct SpeedLimiter {
     duration_between_action: Duration,
     last_time: Instant,
+    loop_delay: Duration
 }
 
 impl SpeedLimiter {
@@ -12,12 +14,16 @@ impl SpeedLimiter {
         SpeedLimiter {
             duration_between_action: frames_per_duration,
             last_time: Instant::now(),
+            loop_delay: Duration::from_millis(5)
         }
     }
     pub fn tick<F: FnMut() -> ()>(&mut self, mut f: F) {
         if Instant::now() - self.last_time >= self.duration_between_action {
             f();
             self.last_time = Instant::now();
+        }
+        else {
+            sleep(self.loop_delay)
         }
     }
 }
