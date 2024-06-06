85a8346c57af47d8c41f697cb77184ddc64bc5b1
Lewis <lsm10100@gmail.com>
2024-06-06 22:43:42 +0100

    refactor movements

--- src/shapes.rs
+++ src/shapes.rs
@@ -6,40 +6,63 @@ pub struct BlockGroup {
 
 impl BlockGroup {
     fn move_down(&mut self, other_blocks_in_scene: &[TetrisBlock]) -> Result<(),()> {
-        if self.can_move(other_blocks_in_scene, 0, -1) {
-            self.blocks.iter_mut().for_each(|b| b.y -= 1);
-            return Ok(());
+        let new_positions = [
+            self.blocks[0].moved(0, -1),
+            self.blocks[1].moved(0, -1),
+            self.blocks[2].moved(0, -1),
+            self.blocks[3].moved(0, -1)
+        ];
+
+        if self.can_move(other_blocks_in_scene, &new_positions) {
+            self.blocks = new_positions;
+            Ok(())
         }
-        Err(())
+        else
+        {
+            Err(())
+        }
     }
 
     fn move_left(&mut self, other_blocks_in_scene: &[TetrisBlock]) {
-        if self.can_move(other_blocks_in_scene, -1, 0) {
-            self.blocks.iter_mut().for_each(|b| b.x -= 1)
-        }
+        let new_positions = [
+            self.blocks[0].moved(-1, 0),
+            self.blocks[1].moved(-1, 0),
+            self.blocks[2].moved(-1, 0),
+            self.blocks[3].moved(-1, 0)
+        ];
 
+        if self.can_move(other_blocks_in_scene, &new_positions) {
+            self.blocks = new_positions;
+        }
     }
     fn move_right(&mut self, other_blocks_in_scene: &[TetrisBlock]) {
-        if self.can_move(other_blocks_in_scene, 1, 0) {
-            self.blocks.iter_mut().for_each(|b| b.x += 1)
+        let new_positions = [
+            self.blocks[0].moved(1, 0),
+            self.blocks[1].moved(1, 0),
+            self.blocks[2].moved(1, 0),
+            self.blocks[3].moved(1, 0)
+        ];
+
+        if self.can_move(other_blocks_in_scene, &new_positions) {
+            self.blocks = new_positions;
         }
     }
 
-    fn can_move(&self, other_blocks_in_scene: &[TetrisBlock], x_movement: i32, y_movement: i32) -> bool {
-        for current_block_to_move in &self.blocks {
-            if current_block_to_move.y + y_movement == -1 {
+    fn can_move(&self, other_blocks_in_scene: &[TetrisBlock], new_position: &[TetrisBlock]) -> bool {
+        for current_block_to_move in new_position {
+            if current_block_to_move.y == -1 {
                 return false;
             }
-            if current_block_to_move.x + x_movement == -1 {
+            if current_block_to_move.x == -1 {
                 return false;
             }
-            if current_block_to_move.x + x_movement == 10 {
+            if current_block_to_move.x == 10 {
                 return false;
             }
 
             for block_in_scene in other_blocks_in_scene {
-                if current_block_to_move.x + x_movement == block_in_scene.x &&
-                    current_block_to_move.y + y_movement == block_in_scene.y {
+                if current_block_to_move.x == block_in_scene.x &&
+                    current_block_to_move.y == block_in_scene.y {
                     return false;
                 }
             }
